814a1210781474b8a8efa634bb63496642d6b1b2
noranokyoju_256 <norakyo256@gmail.com>
2021-10-08 13:35:58 +0900

    add new file

---
+++ Assets/Editor/RewiredConstantsCreator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace Norakyo.Internal
+{
+	internal sealed class RewiredConstantsCreator : EditorWindow
+	{
+		Rewired.InputManager inputManager;
+		[MenuItem("Tools/Yasukou/Constants")]
+		static void Open()
+		{
+			EditorWindow.GetWindow<RewiredConstantsCreator>("RewiredConstantsCreator");
+		}
+
+		private void OnGUI()
+		{
+			inputManager = (Rewired.InputManager)EditorGUILayout.ObjectField("ObjectField", inputManager, typeof(Rewired.InputManager), true);
+			if (GUILayout.Button("Create") && inputManager != null)
+			{
+				var userData = inputManager.userData;
+				Dictionary<string, int> actionDic = new Dictionary<string, int>();
+				foreach (var id in userData.GetActionIds())
+				{
+					actionDic[userData.GetActionNameById(id)] = id;
+				}
+				ConstantsClassCreator.Create("ActionID", "Rewiredのアクション番号を定数で管理するクラス", actionDic);
+			}
+		}
+	}
+}
+
